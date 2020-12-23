147c94606b698e45c6ed73cab0e1a7a9de94e8fb
AmerBiro <amer.biro@gmail.com>
2020-12-23 12:36:13 +0100

    Adding the needed packages for the project. Adding the needed Fragments for Registration

---
+++ app/src/main/java/com/example/fitnessapp/registration/Membership.java
@@ -0,0 +1,22 @@
+package com.example.fitnessapp.registration;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.fitnessapp.R;
+
+public class Membership extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.registration_membership, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/fitnessapp/registration/Registration.java
@@ -0,0 +1,23 @@
+package com.example.fitnessapp.registration;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.fitnessapp.R;
+
+
+public class Registration extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.registration_registration, container, false);
+    }
+
+}
