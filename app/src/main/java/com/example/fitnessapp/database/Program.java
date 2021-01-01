36e9916b8cb736bdf301b5e1f3d908106caf2295
AmerBiro <amer.biro@gmail.com>
2021-01-01 20:15:22 +0100

    Adding create program class

--- app/src/main/java/com/example/fitnessapp/database/Program.java
+++ app/src/main/java/com/example/fitnessapp/database/Program.java
@@ -1,4 +1,56 @@
 package com.example.fitnessapp.database;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Program {
+
+    private String program_name, program_name_key,
+            days, days_key,
+            exercises, exercises_key,
+            start_date, start_date_key,
+            end_date, end_date_key;
+
+    public Program(String program_name, String program_name_key,
+                   String days, String days_key,
+                   String exercises, String exercises_key,
+                   String start_date, String start_date_key,
+                   String end_date, String end_date_key) {
+        this.program_name = program_name;
+        this.program_name_key = program_name_key;
+        this.days = days;
+        this.days_key = days_key;
+        this.exercises = exercises;
+        this.exercises_key = exercises_key;
+        this.start_date = start_date;
+        this.start_date_key = start_date_key;
+        this.end_date = end_date;
+        this.end_date_key = end_date_key;
+    }
+
+    public void createProgram() {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser userID = firebaseAuth.getCurrentUser();
+        String userId = userID.getUid();
+        CollectionReference programRef = FirebaseFirestore.getInstance()
+                .collection("users").document(userId)
+                .collection("ProgramList");
+
+        Map<String, Object> program = new HashMap<>();
+
+        program.put(program_name_key, this.program_name);
+        program.put(days_key, this.days);
+        program.put(exercises_key, this.exercises);
+        program.put(start_date_key, this.start_date);
+        program.put(end_date_key, this.end_date);
+
+        programRef.add(program);
+
+    }
+
 }
